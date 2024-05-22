d0bbd76cf071d452b212988f378dac14c9a39103
tom-mate-o <132936694+tom-mate-o@users.noreply.github.com>
2024-05-22 13:45:02 +0200

    agenda proton test

--- src/components/Agenda.jsx
+++ src/components/Agenda.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { Calendar } from '@fullcalendar/core';
+import iCalendarPlugin from '@fullcalendar/icalendar';
 import listPlugin from '@fullcalendar/list';
 import nlLocale from '@fullcalendar/core/locales/nl';
 
@@ -9,14 +10,14 @@ export default function Agenda() {
   useEffect(() => {
     const calendarEl = calendarRef.current;
     const calendar = new Calendar(calendarEl, {
-      plugins: [listPlugin],
+      plugins: [listPlugin, iCalendarPlugin],
       initialView: 'listWeek',
       locale: nlLocale,
       contentHeight: 'auto',
 
       events: {
-        url: '/.netlify/functions/fetch-events',
-        format: 'json',
+        url: 'https://calendar.proton.me/api/calendar/v1/url/bi_quJKrAd_Ko9erjojgkCWouAKFnOFcIoUcvA8zF0lCIOB351AWumQPHzy92gOA1uDFxtiGS9aF4TC4WOBvbg==/calendar.ics?CacheKey=IOkIe0kodCaKEGZAmsV6dg%3D%3D&PassphraseKey=tHVkXG3vHn8daSBuUnEdV7ImzMGifZ12GIVL-U4egV4%3D',
+        format: 'ics',
       },
     });
 
