46d94d26f2ee421c48237d8314d930661764cd1b
tom-mate-o <132936694+tom-mate-o@users.noreply.github.com>
2024-05-23 09:10:41 +0200

    update

--- src/pages/agendapage.js
+++ src/pages/agendapage.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import Agenda from '../components/Agenda.jsx';
 import { Link } from 'react-router-dom';
 import Calendly from '../components/Calendly.jsx';
+import { BsWhatsapp } from 'react-icons/bs';
 
 export default function AgendaPage() {
   useEffect(() => {
@@ -25,6 +26,16 @@ export default function AgendaPage() {
           </p>
           <button className="button cta">
             <Link to="/contact">Maak een afspraak</Link>
+          </button>
+          <button className="button whatsapp">
+            <a
+              href="https://wa.me/31648149616?text=Hoi,%20Magic%20Nonsense!"
+              target="_blank"
+            >
+              <span>
+                <BsWhatsapp /> WhatsApp ons
+              </span>
+            </a>
           </button>
         </div>
       </div>
